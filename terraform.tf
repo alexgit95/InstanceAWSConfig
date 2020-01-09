47203073c55324030e65d763836799015cd502fb
Alex B <BOURGOIS_A@id.fr.cly>
2020-01-09 11:00:54 +0100

    First commit

---
+++ terraform.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "${var.region-aws}"
+  shared_credentials_file = "${var.shared_cred_file}"
+}
+
+
+
+resource "aws_instance" "moninstanceec2" {
+  ami = "ami-087855b6c8b59a9e4"
+  instance_type = "t2.micro"
+  count = 1
+  tags = {
+    Owner = "alex"
+    Name = "${var.nom_instance}"
+  }
+}
+
+resource "aws_security_group" "moninstanceec2"{
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    protocol = "tcp"
+    to_port = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
+
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "shared_cred_file" {
+  default = "C:/Users/bulbi/.aws/credentials"
+}
+
+variable "region-aws" {
+  type = string
+  default = "eu-west-3"
+}
+
+variable "nom_instance" {
+  type = string
+  default = "srv-docker-alex"
+}
