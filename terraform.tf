47406b22e9dda9bdc34b7748c67fa0fafba545bd
Alex B <>
2020-02-01 19:18:59 +0100

    Ajout du portail, configurtaion pour que les port ne soit ouvert qu'à l'interieur du security group, et seul le portail porte le certificat SSL, l'adresse de communciation interservice est l'adresse prive pour rester à l'interieur du security group

--- terraform.tf
+++ terraform.tf
@@ -54,12 +54,22 @@ resource "aws_security_group" "moninstanceec2"{
     from_port = 8888
     protocol = "tcp"
     to_port = 8888
-    cidr_blocks = ["0.0.0.0/0"]
+    /*cidr_blocks = ["0.0.0.0/0"]*/
+    self = true
   }
   ingress {
     from_port = 8899
     protocol = "tcp"
     to_port = 8899
+   /* cidr_blocks = ["0.0.0.0/0"]*/
+    self = true
+  }
+
+
+  ingress {
+    from_port = 443
+    protocol = "tcp"
+    to_port = 443
     cidr_blocks = ["0.0.0.0/0"]
   }
   
@@ -90,6 +100,13 @@ resource "null_resource" "example2" {
   provisioner "local-exec" {
 
     command = "echo '[all]\n${aws_eip_association.moninstanceec2.public_ip}\n[all:vars]\nansible_ssh_private_key_file=ssh/id_rsa' > hosts"
+  }
+}
+
+resource "null_resource" "configportail" {
+  provisioner "local-exec" {
+
+    command = "echo 'ip.geoposition=${aws_eip_association.moninstanceec2.private_ip_address}\nport.geoposition=8899\nip.myphotos=${aws_eip_association.moninstanceec2.private_ip_address}\nport.myphotos=8888' > /root/properties/configuration-portail.properties"
   }
 }
 
